839bc441e7910986c894144f59fb0d2d3ea53f51
Sarra <sarrasouissi13@gmail.com>
2026-05-17 15:20:35 +0200

    my first commit

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen">
+
+      {/* NAVBAR */}
+      <nav className="flex justify-between items-center px-10 py-6 border-b border-gray-800">
+        <h1 className="text-xl font-bold">Sarra</h1>
+
+        <ul className="flex gap-6 text-gray-300">
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      {/* HERO */}
+      <section className="flex flex-col items-center justify-center text-center h-[80vh]">
+        <h2 className="text-5xl font-bold mb-4">
+          Hello, Im Sarra 👋
+        </h2>
+
+        <p className="text-gray-400 text-xl">
+          Future Full Stack Developer
+        </p>
+
+        <button className="mt-6 px-6 py-3 bg-white text-black rounded-full">
+          See My Work
+        </button>
+      </section>
+
+    </main>
+  );
+}
+
+
+
+
+
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+//       <h1 className="text-6xl font-bold mb-4">
+//         Sarra Portfolio ✨
+//       </h1>
+
+//       <p className="text-gray-400 text-xl">
+//         Future Full Stack Developer
+//       </p>
+//     </main>
+//   );
+// }
