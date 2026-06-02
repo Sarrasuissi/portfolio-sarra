2cb748e845a5de221acd8e6e2f5106031d01fff2
Sarrasuissi <sarrasouissi13@gmail.com>
2026-06-02 10:57:43 +0100

    header 1

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,23 @@
-// import Link from "next/link";
+import Link from "next/link";
 
-// export default function NotFound() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white px-6">
-//       <h1 className="text-7xl font-bold mb-4">404</h1>
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white px-6">
+      <h1 className="text-7xl font-bold mb-4">404</h1>
 
-//       <p className="text-zinc-400 text-lg mb-8 text-center">
-//         Oups... cette page nexiste pas.
-//       </p>
+      <p className="text-zinc-400 text-lg mb-8 text-center">
+        Oups... cette page nexiste pas.
+      </p>
 
-//       <Link
-//         href="/"
-//         className="rounded-xl bg-white px-6 py-3 text-black font-medium transition hover:bg-zinc-200"
-//       >
-//         Retour à laccueilccccccccccccccccccccccccccc
-//       </Link>
-//     </main>
-//   );
-// }
+      <Link
+        href="/"
+        className="rounded-xl bg-white px-6 py-3 text-black font-medium transition hover:bg-zinc-200"
+      >
+        Retour à laccueilccccccccccccccccccccccccccc
+      </Link>
+    </main>
+  );
+}
 
 
 
